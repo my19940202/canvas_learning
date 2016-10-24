815c06c9cdd7e2fbfc9774848fec3546d0230992
baidu <baidu@xishengbos-MacBook-Pro.local>
2016-10-24 09:57:40 +0800

    暂时还未解决hitTest

--- app/step3.js
+++ app/step3.js
@@ -6,22 +6,20 @@ const canvasSize = {
     height: 800
 };
 
-//创建一个舞台，得到一个参考的画布
+// 创建一个舞台
 let stage = new createjs.Stage("myCanvas");
-//创建一个形状的显示对象
+// 创建一个形状的显示对象
 let rect = new createjs.Shape();
 rect.graphics.beginFill("blue").drawRect(0, 0, 40, 40);
 let circle = new createjs.Shape();
-circle.graphics.beginFill('red').drawCircle(-10, -10, 20);
+circle.graphics.beginFill('black').drawCircle(200, 200, 20);
 stage.addChild(circle);
-//形状实例的设置位置
+// 形状实例的设置位置
 rect.x = rect.y = 50;
-//添加形状实例到舞台显示列表
+// 添加形状实例到舞台显示列表
 stage.addChild(rect);
-//更新舞台将呈现下一帧
+// 更新舞台将呈现下一帧
 stage.update();
-
-
 
 let direct = 'right';
 let moveOneStep = function () {
@@ -50,8 +48,10 @@ let moveOneStep = function () {
                 rect.y += moveStep;
             }
             break;
-    }
+    };
+    rect.setTransform(rect.x, rect.y);
     stage.update();
+    console.log(rect.hitArea)
 };
 
 let timer = setInterval(function () {
@@ -61,6 +61,7 @@ let setDiret = function (param) {
     direct = param;
 };
 
+/*
 let generateFruit = function () {
     let fruitX = Math.floor(Math.random() * 800);
     let fruitY = Math.floor(Math.random() * 800);
@@ -68,9 +69,8 @@ let generateFruit = function () {
     circle.y = fruitY;
     stage.update();
 };
-
 let fruitTimer = setInterval(generateFruit, 1000);
-
+*/
 
 
 document.onkeydown = function (event) {  
@@ -81,24 +81,27 @@ document.onkeydown = function (event) {
     } else {
         key = event.which;
     }
-    if(key == 37){
+    if (key == 37) {
         setDiret('left');
-        console.log('left');
     }
-    if(key == 38){
+    if (key == 38) {
         setDiret('top');
-        console.log('top');
     }
-    if(key == 39){
+    if (key == 39) {
         setDiret('right');
-        console.log('right');
     }
-    if(key == 40){
+    if (key == 40) {
         setDiret('down');
-        console.log('down');
     }
 }
 
+
+// var myShape = new createjs.Shape();
+// myShape.graphics.beginFill('blue').drawRect(100, 100, 20, 50);
+// stage.addChild(myShape);
+// stage.update();
+// console.log(myShape.hitTest(10,10));
+// console.log(myShape.hitTest(110, 125));
 
 
 let step3 = {};
